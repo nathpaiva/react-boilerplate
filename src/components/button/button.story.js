d7d5a53d1f72ad0722368ec096b2be31fa02c260
Nath Paiva <hello@nathpaiva.com.br>
2018-08-08 11:11:17 -0300

    Remove comments line

--- src/components/button/button.story.js
+++ src/components/button/button.story.js
@@ -2,17 +2,9 @@
 
 import React from 'react';
 import { storiesOf } from '@storybook/react';
-// import { action } from '@storybook/addon-actions';
 
 import '../../css/_reset.scss';
 import Button from './';
-
-// const props = {
-//   info: 'tooltip',
-//   visible: false,
-//   trigger: select('trigger', ['hover', 'focus'], 'hover'),
-//   onVisibleChange: linkTo('Tooltip', 'opened')
-// };
 
 storiesOf('Special Button', module)
   .add('default', () => <Button />)
