aa4a37681498d8f8c8a927c6d860ae0748189f32
Nath Paiva <hello@nathpaiva.com.br>
2018-08-08 12:27:50 -0300

    Config addons to config storybook

--- .storybook/config.js
+++ .storybook/config.js
@@ -1,11 +1,26 @@
 'use strict';
 
 import { configure } from '@storybook/react';
+import { setOptions } from '@storybook/addon-options';
 
 const req = require.context('../src/components', true, /\.story\.js$/);
 
 function loadStories() {
   req.keys().forEach(filename => req(filename));
 }
+
+// Option defaults:
+setOptions({
+  /**
+   * name to display in the top left corner
+   * @type {String}
+   */
+  name: 'Components of app',
+  /**
+   * URL for name in top left corner to link to
+   * @type {String}
+   */
+  url: 'http://nathpaiva.com.br',
+});
 
 configure(loadStories, module);
